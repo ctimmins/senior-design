a146b9632c7f6860ca41df7bd79dfd9d34413f2b
Chad Timmins <chadtimmins@gmail.com>
2015-01-15 14:32:31 -0800

    ignore config file

---
+++ Agricultural Monitoring System/Alpha Prototype/GUI/scripts/app.js
@@ -0,0 +1,27 @@
+var app = {
+
+	initialize: function() {
+		app.baseURL = 'http://login.etherios.com';
+	},
+
+	checkStatus: function () {
+
+	},
+
+	getDeviceInfo: function (deviceID) {
+
+	},
+
+	getSettings: function () {
+
+	},
+
+	getAllData: function () {
+
+	},
+
+	getOneData: function (deviceID) {
+
+	}
+
+};
