10f852f604b1b28106d602c01d02c1233f32268c
Chad Timmins <chadtimmins@gmail.com>
2015-01-15 16:51:51 -0800

    separated app functionality from digi device commands and other functions

--- Agricultural Monitoring System/Alpha Prototype/GUI/scripts/app.js
+++ Agricultural Monitoring System/Alpha Prototype/GUI/scripts/app.js
@@ -6,6 +6,19 @@ var app = {
 
 	},
 
+};
+
+var Digi = {
+
+	baseURL: 'http://login.etherios.com',
+
+	/**
+	 * check status of ConnectPort(s)
+	 * @param  {string} deviceID mac address of ConnectPort
+	 * @param  {string} user     username for Device Cloud account
+	 * @param  {string} pw       password for Device Cloud account
+	 * @return {xml}           
+	 */
 	checkStatus: function (deviceID, user, pw) {
 
 		var headers = {
@@ -13,12 +26,19 @@ var app = {
 			'Content-Type': 'text/xml; charset="UTF-8"'
 		}
 
-		var payload = digiMessage.queryState(deviceID);
+		var payload = Digi.message.queryState(deviceID);
 
-		app.post(baseURL+'/ws/sci', payload, headers);
+		Digi.post(Digi.baseURL+'/ws/sci', payload, headers);
 
 	},
 
+	/**
+	 * get device information of ConnectPort(s)
+	 * @param  {string} deviceID mac address of ConnectPort
+	 * @param  {string} user     username for Device Cloud account
+	 * @param  {string} pw       password for Device Cloud account
+	 * @return {xml}
+	 */
 	getDeviceInfo: function (deviceID, user, pw) {
 		
 		var headers = {
@@ -28,18 +48,37 @@ var app = {
 
 	},
 
-	getSettings: function () {
+	/**
+	 * get device settings of ConnectPort
+	 * @param  {string} deviceID mac address of ConnectPort
+	 * @param  {string} user     username for Device Cloud account
+	 * @param  {string} pw       password for Device Cloud account
+	 * @return {xml}
+	 */
+	getSettings: function (deviceID, user, pw) {
+		var headers = {
+			'Authorization': 'Basic ' + user +':' + pw,
+			'Content-Type': 'text/xml; charset="UTF-8"'
+		}
 
+		var payload = Digi.message.querySetting(deviceID);
 	},
 
 	getAllData: function () {
 
 	},
 
-	getOneData: function (deviceID) {
+	getOneDataStream: function (deviceID) {
 
 	},
 
+	/**
+	 * HTTP POST method for device cloud with custom headers
+	 * @param  {string} url     url for http request
+	 * @param  {string} data    data to send to url
+	 * @param  {json} headers   custom headers for authentication
+	 * @return {[type]}         [description]
+	 */
 	post: function(url, data, headers) {
 		$.ajax({
 			beforeSend: function (xhr) {
@@ -55,44 +94,43 @@ var app = {
 		});
 	}
 
-};
+	/**
+	 * various messages for digi device cloud commands
+	 * @type {Object}
+	 */
+	message = {
 
-/**
- * returns the xml string for http request needed for various device cloud operations
- * @type {Object}
- */
-var digiMessage = {
+		queryState: function (target) {
+			var message = 
+						'<sci_request version = "1.0"' +
+						  	'<send_message>' +
+							  	'<targets>' +
+							  		'<device id="'+ target +'"/>' +
+						  		'</targets>' +
+						  		'<rci_request version="1.1"' +
+						  			'<query_state/>' +
+					  			'</rci_request>' +
+				  			'</send_message>' +
+			  			'</sci_request>';
+			
+			return message;
+		},
 
-	queryState: function (target) {
-		var message = 
-					'<sci_request version = "1.0"' +
-					  	'<send_message>' +
-						  	'<targets>' +
-						  		'<device id="'+ target +'"/>' +
-					  		'</targets>' +
-					  		'<rci_request version="1.1"' +
-					  			'<query_state/>' +
-				  			'</rci_request>' +
-			  			'</send_message>' +
-		  			'</sci_request>';
-		
-		return message;
-	},
+		querySetting: function (target) {
+			var message = 
+						'<sci_request version = "1.0"' +
+						  	'<send_message>' +
+							  	'<targets>' +
+							  		'<device id="'+ target +'"/>' +
+						  		'</targets>' +
+						  		'<rci_request version="1.1"' +
+						  			'<query_setting/>' +
+					  			'</rci_request>' +
+				  			'</send_message>' +
+			  			'</sci_request>';
+			
+			return message;
+		},
 
-	querySetting: function (target) {
-		var message = 
-					'<sci_request version = "1.0"' +
-					  	'<send_message>' +
-						  	'<targets>' +
-						  		'<device id="'+ target +'"/>' +
-					  		'</targets>' +
-					  		'<rci_request version="1.1"' +
-					  			'<query_setting/>' +
-				  			'</rci_request>' +
-			  			'</send_message>' +
-		  			'</sci_request>';
-		
-		return message;
-	},
-
+	};
 }
