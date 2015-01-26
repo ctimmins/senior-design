94341c59ff8a11f6e5aa54cad729fad99ba25c21
Chad Timmins <chadtimmins@gmail.com>
2015-01-26 10:43:09 -0800

    fixed python comment issues in nic's script

--- Agricultural Monitoring System/Alpha Prototype/ConnectPort/nic.py
+++ Agricultural Monitoring System/Alpha Prototype/ConnectPort/nic.py
@@ -17,14 +17,14 @@ var = time.localtime(time.time());
 # Store length # of bytes in buffer_in and the XBee source address in src_addr
 # NOTE: if there are more bytes on the receive buffer than length, extra bytes will be discarded
 # NOTE: socket is blocking by default
-buffer_in, src_addr = sd.recvfrom(length);
+buffer_in, src_addr = sd.recvfrom(255);
 # To make the socket non-blocking:
 sd.setblocking(0);
 # To return to blocking:
 sd.setblocking(1);
 
 # Send character 'g' to XBee with address src_addr
-sd.sendto('g',0,src_addr);
+sd.sendto('g',0,'00:13:a2:00:40:c2:5c:7b!');
 
 
 
