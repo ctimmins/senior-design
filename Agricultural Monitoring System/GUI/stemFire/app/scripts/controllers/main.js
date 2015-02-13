845c43167fe319e807f542181e38e0cab45f1f1f
Chad Timmins <chadtimmins@gmail.com>
2015-02-12 18:02:10 -0800

    changed method of iterating through snapshot using Firebase SDK

--- Agricultural Monitoring System/GUI/stemFire/app/scripts/controllers/main.js
+++ Agricultural Monitoring System/GUI/stemFire/app/scripts/controllers/main.js
@@ -10,22 +10,35 @@
 angular.module('stemFireApp')
   .controller('MainCtrl', function ($scope, fbutil) {
 
-    $scope.nodeData = fbutil.syncObject('Test/2');
+    $scope.nodeData = fbutil.syncArray('Test/2', {limitToLast: 10});
     
-    var nodeRef = fbutil.ref('Test/2');
-    nodeRef.orderByKey().on('child_added', function(snapshot){
-    	console.log(snapshot.key());
-    	var sensorRef = nodeRef.child(snapshot.key());
-    	sensorRef.on('child_added', function(s_snapshot){
-    		console.log('  sensor height: ' + s_snapshot.key());
-    		var valRef = sensorRef.child(s_snapshot.key());
-    		valRef.on('child_added', function(v_snapshot){
-    			console.log('          ' + v_snapshot.key() + ': ' + v_snapshot.val());
-    		});
-    		console.log('');
+    $scope.snapshot = [];
 
+    var nodeRef = $scope.nodeData.$inst().$ref();
+
+    nodeRef.orderByKey().on('child_added', function(snapshot) {
+    	var obj = {};
+    	obj[snapshot.key()] = snapshot.exportVal(); 
+    	$scope.snapshot.push(obj);
+    	console.log(snapshot.key());
+    	snapshot.forEach(function(child) {
+    		console.log(child.key() + ': ' + JSON.stringify(child.exportVal()));
     	});
-    	console.log('-------------------');
+
+    	console.log('');
+    	console.log($scope.snapshot);
+    	console.log('-------------------')
+    	
+
     });
+
+    nodeRef.on('child_removed', function(snapshot) {
+    	var index = $scope.snapshot.indexOf(snapshot.key());
+    	if (index > -1) {
+    		$scope.snapshot.splice(index, 1);
+    	}
+    });
+
+ 
     
   });
