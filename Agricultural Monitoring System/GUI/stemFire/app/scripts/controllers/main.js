5b1fdef1f65452983b9d333fb3ff098e0959790a
Chad Timmins <chadtimmins@gmail.com>
2015-02-22 00:48:16 -0800

    hard coded more data for performance measurements

--- Agricultural Monitoring System/GUI/stemFire/app/scripts/controllers/main.js
+++ Agricultural Monitoring System/GUI/stemFire/app/scripts/controllers/main.js
@@ -33,94 +33,261 @@ angular.module('stemFireApp')
     });
 
     // Angular Chart
-    $scope.dataset = [
+
+    $scope.chartType = 'area-spline';
+
+    $scope.vwc = [
       {
-        'day': '2013-01-08T00:00:00',
-        'vwc': 300,
-        'income': 200,
-        'customers': 30,
-        'units': 130,
-        'dayString': 'Montag'
+        'day': '2015-2-21 00:00:00',
+        '0': 39,
+        '1': 21,
+        '2': 26,
+        '3': 12,
+        '4': 17,
+        '5': 11
       },
       {
-        'day': '2013-01-03T00:00:00',
-        'vwc': 200,
-        'income': 130,
-        'customers': 20,
-        'units': 120,
-        'dayString': 'Dienstag'
+        'day': '2015-2-21 00:05:00',
+        '0': 36,
+        '1': 22,
+        '2': 24,
+        '3': 10,
+        '4': 18,
+        '5': 13
       },
       {
-        'day': '2013-01-04T00:00:00',
-        'vwc': 160,
-        'income': 90,
-        'customers': 50,
-        'units': 150,
-        'dayString': 'Mittwoch'
+        'day': '2015-2-21 00:10:00',
+        '0': 34,
+        '1': 28,
+        '2': 21,
+        '3': 9,
+        '4': 17,
+        '5': 13
       },
       {
-        'day': '2013-01-05T00:00:00',
-        'vwc': 400,
-        'income': 240,
-        'customers': 40,
-        'units': 140,
-        'dayString': 'Donnerstag'
+        'day': '2015-2-21 00:15:00',
+        '0': 30,
+        '1': 27,
+        '2': 19,
+        '3': 8,
+        '4': 19,
+        '5': 14
       },
       {
-        'day': '2013-01-06T00:00:00',
-        'vwc': 250,
-        'income': 130,
-        'customers': 60,
-        'units': 160,
-        'dayString': 'Freitag'
+        'day': '2015-2-21 00:20:00',
+        '0': 26,
+        '1': 22,
+        '2': 18,
+        '3': 8,
+        '4': 18,
+        '5': 16
       },
       {
-        'day': '2013-01-07T00:00:00',
-        'vwc': 250,
-        'income': 220,
-        'customers': 50,
-        'units': 150,
-        'dayString': 'Samstag'
+        'day': '2015-2-21 00:25:00',
+        '0': 23,
+        '1': 23,
+        '2': 17,
+        '3': 7,
+        '4': 19,
+        '5': 15
+      },
+      {
+        'day': '2015-2-21 00:30:00',
+        '0': 39,
+        '1': 21,
+        '2': 26,
+        '3': 12,
+        '4': 17,
+        '5': 11
+      },
+      {
+        'day': '2015-2-21 00:35:00',
+        '0': 36,
+        '1': 22,
+        '2': 24,
+        '3': 10,
+        '4': 18,
+        '5': 13
+      },
+      {
+        'day': '2015-2-21 00:40:00',
+        '0': 34,
+        '1': 28,
+        '2': 21,
+        '3': 9,
+        '4': 17,
+        '5': 13
+      },
+      {
+        'day': '2015-2-21 00:45:00',
+        '0': 30,
+        '1': 27,
+        '2': 19,
+        '3': 8,
+        '4': 19,
+        '5': 14
+      },
+      {
+        'day': '2015-2-21 00:50:00',
+        '0': 26,
+        '1': 22,
+        '2': 18,
+        '3': 8,
+        '4': 18,
+        '5': 16
+      },
+      {
+        'day': '2015-2-21 00:55:00',
+        '0': 23,
+        '1': 23,
+        '2': 17,
+        '3': 7,
+        '4': 19,
+        '5': 15
+      },
+      {
+        'day': '2015-2-21 01:00:00',
+        '0': 39,
+        '1': 21,
+        '2': 26,
+        '3': 12,
+        '4': 17,
+        '5': 11
+      },
+      {
+        'day': '2015-2-21 01:05:00',
+        '0': 36,
+        '1': 22,
+        '2': 24,
+        '3': 10,
+        '4': 18,
+        '5': 13
+      },
+      {
+        'day': '2015-2-21 01:10:00',
+        '0': 34,
+        '1': 28,
+        '2': 21,
+        '3': 9,
+        '4': 17,
+        '5': 13
+      },
+      {
+        'day': '2015-2-21 01:15:00',
+        '0': 30,
+        '1': 27,
+        '2': 19,
+        '3': 8,
+        '4': 19,
+        '5': 14
+      },
+      {
+        'day': '2015-2-21 01:20:00',
+        '0': 26,
+        '1': 22,
+        '2': 18,
+        '3': 8,
+        '4': 18,
+        '5': 16
+      },
+      {
+        'day': '2015-2-21 01:25:00',
+        '0': 23,
+        '1': 23,
+        '2': 17,
+        '3': 7,
+        '4': 19,
+        '5': 15
       }
     ];
 
     $scope.schema = {
       day: {
         type: 'datetime',
+        format: '%Y-%m-%d %H:%M:%S',
         name: 'Date'
       },
-      income: {
+      0: {
         type: 'numeric',
-        name: 'income'
-      }
+        name: 'Moisture L0'
+      },
+      1: {
+        type: 'numeric',
+        name: 'Temp L1'
+      },
+      2: {
+        type: 'numeric',
+        name: 'Moisture L2'
+      },
+      3: {
+        type: 'numeric',
+        name: 'Moisture L3'
+      },
+      4: {
+        type: 'numeric',
+        name: 'Temp L4'
+      },
+      5: {
+        type: 'numeric',
+        name: 'Moisture L5'
+      },
     };
 
     $scope.options = {
       rows: [
         {
-          key: 'income',
-          type: 'bar',
+          key: '0',
+          axis: 'y',
+          type: $scope.chartType
         }, 
         {
-          key: 'vwc'
+          key: '1',
+          axis: 'y2'
         },
         {
-          key: 'units'
-        }
+          key: '2',
+          axis: 'y'
+        },
+        {
+          key: '3',
+          axis: 'y'
+        },
+        {
+          key: '4',
+          axis: 'y2'
+        },
+        {
+          key: '5',
+          axis: 'y'
+        },
       ],
 
       legend: {
-        'selector': true
+        'selector': false
       },
 
       xAxis: {
         key: 'day',
-        displayFormat: '%Y-%m-%d'
+        selector: false,
+        displayFormat: '%H:%M:%S'
       },
 
-      type: 'area-spline',
+      yAxis: {
+        label: 'Volumetric Water Content (%)'
+      },
+
+      y2Axis: {
+        label: 'Temperature (C)'
+      },
+
+      type: $scope.chartType,
 
     };
- 
+
+    $scope.updateChartType = function(sel){
+      $scope.options.rows.forEach(function(row) {
+        row.type = sel;
+      });
+    };
     
   });
